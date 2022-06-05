e2c38d9eb9bd5132927a3c93c17bcb2a556e5875
YaredT21 <teklewold.yared21@gmail.com>
2022-06-05 08:08:53 +0300

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-            return (0, None)
-    return (len(sentence), sentence[0])
+    length = len(sentence)
+    first_char = sentence[0] if length > 0 else "None"
+    tup = length, first_char
+    return(tup)
