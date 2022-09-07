2ae6d4f9b9ba5c3c1a489c6335ca3229ac4e33c0
YaredT21 <teklewold.yared21@gmail.com>
2022-09-07 04:24:49 +0300

    completed task

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-if (process.argv.length < 1 ) {
+if (process.argv.length < 0 ) {
   console.log('No arguments');
-} else if (process.argv.length === 1) {
+} else if (process.argv.length === 0) {
   console.log('Argument found');
 } else {
   console.log('Argument found');
